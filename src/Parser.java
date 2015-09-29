b92f91d3bf6676ab88de0203633b0b83a0a141bc
KM WONG <wongkangmin93@gmail.com>
2015-09-30 00:50:48 +0800

    SLAP time and date breakdown, broken down time and date to separate individual strings with 0 paddings

--- src/Parser.java
+++ src/Parser.java
@@ -5,13 +5,9 @@ public class Parser {
 	public ArrayList<String> evaluateInput(String line) {
 
 		ArrayList<String> test = new ArrayList<String>();
- 		String[] commands = new String[10];
+ 		String[] commands = new String[6];
 		commands = line.split(" ");
 
-		// test display
-		for (int i = 0; i < commands.length; i++) {
-			System.out.println(commands[i]);
-		}
 		switch (commands[0]) {
 		case "create": {
 			System.out.println("Create selected");
@@ -45,29 +41,54 @@ public class Parser {
 	public ArrayList<String> create(String[] commands) {
 		ArrayList<String> returnString = new ArrayList<String>();
 		// store the event name
+		returnString.add(commands[0]);
 		returnString.add(commands[1]);
-		// store the start time
-		returnString.add(commands[2]);
-		// edit the date
-		String[] dateBreakdown = new String[3];
-		dateBreakdown = commands[3].split("-");
-		// store the date
-		for (int i = 0; i < dateBreakdown.length; i++) {
-			returnString.add(dateBreakdown[i]);
-		}
-		// store end time
-		returnString.add(commands[4]);
-		// reset dateBreakdown
-		dateBreakdown = new String[3];
-		// edit the end date
-		dateBreakdown = commands[5].split("-");
-		// store the end date
-		for (int i = 0; i < dateBreakdown.length; i++) {
-			returnString.add(dateBreakdown[i]);
-		}
+
+		//edit start time //store start time
+		returnString.addAll(timeBreakdown(commands[2]));
+		
+		// edit start date// store start date
+		returnString.addAll(dateBreakdown(commands[3]));
+		
+		// edit end time // store end time
+		returnString.addAll(timeBreakdown(commands[4]));
+
+		// edit the end date// store the end date
+		returnString.addAll(dateBreakdown(commands[5]));
+		
+
+		
 
 		return returnString;
 
+	}
+	private ArrayList<String> timeBreakdown(String time){
+		ArrayList<String> result = new ArrayList<String>();
+		String[] components = time.split(":");
+		Integer hour = Integer.parseInt(components[0]);
+		if(components[1].contains("am")){
+			components[0] = String.format("%02d", hour);
+			result.add(components[0]);
+		}
+		else{
+			hour+= 12;
+			components[0]  = hour.toString();
+			result.add(components[0]);
+		}
+		components[1] =components[1].replaceAll("[^\\d.]", "");
+		components[1] = String.format("%02d", Integer.parseInt(components[1]));
+		result.add(components[1]);
+		return result;
+	}
+	
+	private ArrayList<String> dateBreakdown(String date){
+		ArrayList<String> result = new ArrayList <String>();
+		String[] components = date.split("-");
+		for (int i = 0 ; i < components.length; i++){
+			components[i] = String.format("%02d", Integer.parseInt(components[i]));
+			result.add(components[i]);
+		}
+		return result; 
 	}
 
 	public ArrayList<String> display(String[] commmands) {
