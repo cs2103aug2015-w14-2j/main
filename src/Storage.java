20cdd0b12d7f258ebd810aafb2728b1c0144c832
Ana <anaprimawaty@gmail.com>
2015-10-04 02:05:14 +0800

    Storage can read from empty files and create non-existent files without error

--- src/Storage.java
+++ src/Storage.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -29,22 +30,30 @@ public class Storage {
 	
 	public ArrayList<AbstractTask> read() {
 		try {
-			FileInputStream in = new FileInputStream("src/storage.txt");
-		  BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-			
 		  ArrayList<AbstractTask> taskList = new ArrayList<AbstractTask>();
-		  String storage = reader.readLine();
-		  String[] tasks = storage.split(",");
-		  for (int i = 0; i < tasks.length; i++) {
-		  	String task = tasks[i];
-		  	String[] taskParts = task.split(" ");
-		  	if (taskParts.length == 1) {
-		  		taskList.add(new FloatingTask(taskParts[0]));
-		  	} else if (taskParts.length == 3) {
-		  		taskList.add(new DeadlineTask(taskParts[0], taskParts[1].replace(":", " "), taskParts[2].replace("-", " ")));
-		  	} else if (taskParts.length == 5) {
-		  		taskList.add(new BoundedTask(taskParts[0], taskParts[1].replace(":", " "), taskParts[2].replace("-", " "), taskParts[3].replace(":", " "), taskParts[4].replace("-", " ")));
-		  	}
+			File file = new File("src/storage.txt");
+		  
+		  if (!file.exists()) {
+		  	file.createNewFile();
+		  }
+		  
+			FileInputStream in = new FileInputStream(file);
+		  BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		  
+		  if (in.available() != 0) {
+			  String storage = reader.readLine();
+			  String[] tasks = storage.split(",");
+			  for (int i = 0; i < tasks.length; i++) {
+			  	String task = tasks[i];
+			  	String[] taskParts = task.split(" ");
+			  	if (taskParts.length == 1) {
+			  		taskList.add(new FloatingTask(taskParts[0]));
+			  	} else if (taskParts.length == 3) {
+			  		taskList.add(new DeadlineTask(taskParts[0], taskParts[1].replace(":", " "), taskParts[2].replace("-", " ")));
+			  	} else if (taskParts.length == 5) {
+			  		taskList.add(new BoundedTask(taskParts[0], taskParts[1].replace(":", " "), taskParts[2].replace("-", " "), taskParts[3].replace(":", " "), taskParts[4].replace("-", " ")));
+			  	}
+			  }
 		  }
 		  
 		  reader.close();
