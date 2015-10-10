4b66cd83d40fc473e45ba61c6f2a90ab34fa9421
Sikai <sikai@lovebyte.us>
2015-10-10 09:11:06 +0800

    Resolve merge conflict for Output.java

---
+++ src/shared/Output.java
@@ -0,0 +1,24 @@
+package shared;
+import java.util.ArrayList;
+
+public class Output {
+	public String returnMessage;
+	public ArrayList<ArrayList<String>> outputArrayList = new ArrayList();
+	
+	public void setReturnMessage(String message) {
+		returnMessage = message;
+	}
+	
+	public String getReturnMessage() {
+		return returnMessage;
+	}
+	
+	public ArrayList<ArrayList<String>> getTasks() {
+		return outputArrayList;
+	}
+	
+	public void setOutput(ArrayList<ArrayList<String>> tasksArrayList) {
+		outputArrayList = tasksArrayList;
+	}
+	
+}
