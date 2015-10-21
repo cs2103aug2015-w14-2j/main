08ab9c6767d2deff4e1454036f7b18bd3e414571
QianPeisheng <a0133888@u.nus.edu>
2015-10-21 15:42:53 +0800

    add fields in output

--- src/shared/Output.java
+++ src/shared/Output.java
@@ -5,6 +5,19 @@ import java.util.Objects;
 public class Output {
 	private String returnMessage;
 	private ArrayList<ArrayList<String>> outputArrayList = new ArrayList();
+	private Priority priority; 
+	private int indexUpdated;
+	public static enum Priority {
+		LOW, HIGH ;
+	}
+	
+	public void setPriority(Priority priority) {
+		this.priority = priority;
+	}
+	
+	public Priority getPriority(Priority priority) {
+		return this.priority;
+	}
 	
 	public void setReturnMessage(String message) {
 		returnMessage = message;
