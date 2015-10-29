be58e057a737055dfa1fe2023bf17874afe64f18
QianPeisheng <a0133888@u.nus.edu>
2015-10-29 14:02:47 +0800

    ui adding bar, changing size

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -39,7 +39,7 @@ public class Main extends Application {
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
-            primaryStage.setHeight(700);
+            primaryStage.setHeight(705);
             primaryStage.setWidth(600);
             primaryStage.setResizable(false);
             primaryStage.show();
