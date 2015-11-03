cb413fd21ff59d599d900dcfde9670560fc9f345
Sikai <sikai93@hotmail.com>
2015-11-04 04:05:35 +0800

    Object that acts as a boolean but can be set using its own methods

---
+++ src/logic/ExtendedBoolean.java
@@ -0,0 +1,21 @@
+package logic;
+
+public class ExtendedBoolean {
+	private boolean state;
+	
+	public ExtendedBoolean(boolean state) {
+		this.state = state;
+	}
+	
+	public void setTrue() {
+		this.state = true;
+	}
+	
+	public void setFalse() {
+		this.state = false;
+	}
+	
+	public boolean getState() {
+		return this.state;
+	}
+}
